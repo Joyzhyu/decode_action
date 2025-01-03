6a3f2b8a35a07813610b168f629250138767866c
action <action@github.com>
2025-01-03 11:47:44 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sun Dec 15 2024 16:36:06 GMT+0000 (Coordinated Universal Time)
+//Fri Jan 03 2025 11:47:44 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("伊利-春节");
